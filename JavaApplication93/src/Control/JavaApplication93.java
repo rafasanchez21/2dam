6c006a34b936af26df0a0488bb08cfd9a3bc6951
rafasanchez21 <72165285+rafasanchez21@users.noreply.github.com>
2020-09-30 23:00:11 +0200

    Add files via upload

---
+++ JavaApplication93/src/Control/JavaApplication93.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control;
+
+import Vista.NewClass;
+
+/**
+ *
+ * @author Usuario
+ */
+public class JavaApplication93 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        NewClass clase = new NewClass();
+        clase.obtenerdatos(); 
+        clase.comprobar();
+        
+        
+        
+    }
+    
+}
---
+++ JavaApplication93/src/Vista/NewClass.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vista;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class NewClass {
+    String u;
+    String p;
+    
+    public void obtenerdatos(){
+    Scanner sc = new Scanner (System.in);
+    System.out.println("Introduce el usuario");
+    u = sc.next();
+    System.out.println("Introduce la contraseña");
+    p = sc.next();
+    }
+   
+    public void comprobar(){
+    while(u.equals("rafa") && p.equals("123")){
+        System.out.println("logueo correcto");
+        break;}
+        
+    while(!u.equals("rafa")|| !p.equals("123")){
+        System.out.println("Error, intentalo de nuevo");
+        break;
+    }    
+        }
+    
+    
+    
+
+    @Override
+    public String toString() {
+        return "NewClass{" + "u=" + u + ", p=" + p + '}';
+    }
+
+    public String getU() {
+        return u;
+    }
+
+    public void setU(String u) {
+        this.u = u;
+    }
+
+    public String getP() {
+        return p;
+    }
+
+    public void setP(String p) {
+        this.p = p;
+    }
+    
+    
+        
+    
+    
+    
+    
+  
+    
+       
+    
+}
